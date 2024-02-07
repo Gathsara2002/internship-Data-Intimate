3b5dd5cf76fd8b88284c8ec48a34fa6fac523f57
Gathsara2002 <liviru2002mmg@gmail.com>
2024-02-07 21:40:56 +0530

    db connection created

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource products');
+    res.send('respond with a resource');
 });
+
+/* GET all products. */
+/*router.get('/', products.getAllProducts);
+
+router.post('/save', products.saveProduct);
+
+router.get('/find/:id', products.searchProduct);
+
+router.delete('/delete/:id', products.deleteProduct);
+
+router.put('/update/:id', products.updateProduct);*/
 
 module.exports = router;
